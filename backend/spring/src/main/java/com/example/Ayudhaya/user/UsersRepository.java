c0d4a7ea99c647f51af77e508034369de7a928bf
Jitrin Panitdirake <jitrinpan@mathregit.group>
2020-08-13 16:16:48 +0700

    [Create] package user

--- backend/spring/src/main/java/com/example/Ayudhaya/user/UsersRepository.java
+++ backend/spring/src/main/java/com/example/Ayudhaya/user/UsersRepository.java
@@ -1,2 +1,7 @@
-package com.example.Ayudhaya.user;public interface UsersRepository {
+package com.example.Ayudhaya.user;
+
+import com.example.Ayudhaya.Country;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UsersRepository  extends MongoRepository<User,String> {
 }
--- backend/spring/src/main/java/com/example/Ayudhaya/user/UsersResponse.java
+++ backend/spring/src/main/java/com/example/Ayudhaya/user/UsersResponse.java
@@ -1,2 +1,77 @@
-package com.example.Ayudhaya.user;public class UsersResponse {
+package com.example.Ayudhaya.user;
+
+public class UsersResponse {
+    private String title;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String beneficial;
+    private String email;
+    private String mobileNumber;
+
+    public UsersResponse() {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.beneficial = beneficial;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setBeneficial(String beneficial) {
+        this.beneficial = beneficial;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getBeneficial() {
+        return beneficial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
 }
