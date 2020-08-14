1551b48ee8099bd39c434e4000e4b5114b762e33
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-08-14 15:30:32 +0700

    [Edited] add more CSS decoration of page1

--- frontend/angular/src/app/insurance/main-insurance/main-insurance.component.ts
+++ frontend/angular/src/app/insurance/main-insurance/main-insurance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CentralStoreService } from 'src/app/service/central-store/central-store.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-insurance',
@@ -9,13 +11,21 @@ export class MainInsuranceComponent implements OnInit {
 
   toSummary: boolean = false;
 
-  constructor() { }
+  constructor(private centralStore: CentralStoreService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onConfirm() {
     this.toSummary = true;
+  }
+
+  confirmInsurance() {
+    this.router.navigateByUrl('/traveller');
+  }
+
+  backInsurance() {
+    this.toSummary = false;
   }
 
 }
